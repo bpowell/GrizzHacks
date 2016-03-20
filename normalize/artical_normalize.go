7c5dc0f4234fac16777966d2046ab9242085c2fc
Erik <eculberson12@gmail.com>
2016-03-20 04:01:40 -0400

    Basic parser for articals

--- normalize/artical_normalize.go
+++ normalize/artical_normalize.go
@@ -6,11 +6,11 @@ import (
 	"strings"
 )
 
-func main() {
+/*
+func _main() {
 	ArticalUniqeWords(test_string)
 }
-
-//var blacklist []string = []string{".", ")", "(", ",", "-"}
+*/
 
 var convertion_table map[string]string = map[string]string{
 	".": "",
@@ -22,20 +22,6 @@ var convertion_table map[string]string = map[string]string{
 	"“": "",
 	"”": "",
 }
-
-var test_string string = `When Alphabet (GOOGL) released Q4 earnings in February, the tech giant revealed that last year it spent $3.6 billion on so-called moonshot projects ranging from self-driving cars to extending human lifespans.
-
-While the potential payoff for these initiatives is years away, investors may profit sooner if the stock breaks out of its current base.
-
-Taking Stock Of Alphabet’s Initiatives
-
-Alphabet generated the bulk of its $75 billion in 2015 revenue from advertising, with ads accounting for 90% of Q4 sales. But as its decision to put robot maker Boston Dynamics up for sale due to a lack of commercial prospects indicates, the sultan of search is serious about monetizing its initiatives. Here’s a quick overview of selected projects.
-
-Self-Driving Cars: Alphabet wants Congress to grant it expedited permission to bring self-driving cars to market — vehicles with no pedals or steering wheels.  As the San Jose Mercury News reported, the move comes as Alphabet is dropping “increasingly strong hints” that autonomous vehicles may be ready to roll sooner than expected.
-
-Cloud Services: The cloud computing market is expected to reach $27.4 billion this year. Google’s Cloud Platform trails Amazon (AMZN) unit Amazon Web Services, which has approximately 37% market share, and Azure from Microsoft (MSFT).  But it may get a boost from Apple (AAPL), which signed a deal worth between $400 million and $600 million to use Google’s Cloud Platform for its iCloud service.
-
-Robotic-Assisted Surgery: Last year, Alphabet’s Verily unit teamed up with Johnson & Johnson (JNJ) to launch Verb Surgical, a joint venture to develop advanced surgical robotics to compete with Intuitive Surgical (ISRG).`
 
 func ArticalUniqeWords(article string) {
 	article = strings.Replace(article, "\n", " ", -1)
